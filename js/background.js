26fef0b346d4c9bc9bb7c5240a82892afa81231c
sewon-swjh0121 <swjh0121@gmail.com>
2018-12-20 01:34:02 +0900

    Fixed again

--- js/background.js
+++ js/background.js
@@ -2,22 +2,21 @@ const body = document.querySelector("body");
 
 const IMG_NUMBER = 3;
 
-function paintImage(imgNumber){
-    const image = new Image();
-    image.src = `${imgNumber + 1}.jpg`;
-    image.classList.add("bgImage");
-    body.appendChild(image);
+function paintImage(imgNumber) {
+  const image = new Image();
+  image.src = `data/${imgNumber + 1}.jpg`;
+  image.classList.add("bgImage");
+  body.appendChild(image);
 }
 
-function genRandom(){
-    const number = Math.floor(Math.random() * 3);
-    return number;
+function genRandom() {
+  const number = Math.floor(Math.random() * 3);
+  return number;
 }
 
-
-function init(){
-    const randomNumber = genRandom();
-    paintImage(randomNumber);
+function init() {
+  const randomNumber = genRandom();
+  paintImage(randomNumber);
 }
 
 init();
