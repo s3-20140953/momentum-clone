e81a2835259fb10f6094173614e2cd7f0f0f0be6
sewon-swjh0121 <swjh0121@gmail.com>
2018-12-31 05:30:55 +0900

    Fix delbutton style

--- js/todo.js
+++ js/todo.js
@@ -1,65 +1,65 @@
 const toDoForm = document.querySelector(".js-toDoForm"),
-toDoInput = toDoForm.querySelector("input"),
-toDoList = document.querySelector(".js-toDo");
+  toDoInput = toDoForm.querySelector("input"),
+  toDoList = document.querySelector(".js-toDo");
 
-const TODOS_LS = "toDos"
+const TODOS_LS = "toDos";
 let toDos = [];
 
-function deleteToDo(e){
-    const btn = e.target;
-    const li = btn.parentNode;
-    toDoList.removeChild(li);
-    const cleanToDos = toDos.filter(function(toDo){
-        return toDo.id !== parseInt(li.id);
+function deleteToDo(e) {
+  const btn = e.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function(toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  delBtn.innerText = " ❌ ";
+  delBtn.addEventListener("click", deleteToDo);
+  const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  span.innerText = text;
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  li.id = newId;
+  toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
+  toDos.push(toDoObj);
+  saveToDos();
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
+}
+
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function(toDo) {
+      paintToDo(toDo.text);
     });
-    toDos = cleanToDos; 
-    saveToDos(); 
+  }
 }
 
-function saveToDos(){
-    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
-}
-
-function paintToDo(text){
-    const li = document.createElement("li");
-    const delBtn = document.createElement("button");
-    delBtn.innerText = "❌ ";
-    delBtn.addEventListener("click",deleteToDo);
-    const span = document.createElement("span");
-    const newId = toDos.length + 1;
-    span.innerText = text;
-    li.appendChild(span);
-    li.appendChild(delBtn);
-    li.id = newId;
-    toDoList.appendChild(li);
-    const toDoObj = {
-        text : text,
-        id : newId
-    };
-    toDos.push(toDoObj);
-    saveToDos();
-}
-
-function handleSubmit(e){
-    e.preventDefault();
-    const currentValue = toDoInput.value;
-    paintToDo(currentValue);
-    toDoInput.value = "";
-}
-
-function loadToDos(){
-    const loadedToDos = localStorage.getItem(TODOS_LS);
-    if(loadedToDos !== null){
-        const parsedToDos = JSON.parse(loadedToDos);
-        parsedToDos.forEach(function(toDo){
-            paintToDo(toDo.text);
-        });
-    }
-}
-
-function init(){
-    loadToDos();
-    toDoForm.addEventListener("submit", handleSubmit);
+function init() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
 }
 
 init();
